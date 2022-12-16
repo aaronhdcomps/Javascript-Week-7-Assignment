ddaedb830c2e4e2bbe2940410c23d5e477b72514
aaronhdcomps <aaron.hdcomps@gmail.com>
2022-12-15 21:30:05 -0800

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,36 @@
 // also want to call this function again when the user saves their preferences to
 // immediately apply them. Make sure you also notify the user somehow that the preferences
 // were saved.
+
+let userName = document.getElementById("name");
+let bgColor = document.getElementById("background-color");
+let txColor = document.getElementById("foreground-color");
+let formFill = document.querySelector("form");
+
+window.addEventListener("load", applyUserPref);
+
+function validateForm() {
+    let empty = document.forms["preferences-form"]["name"].value;
+    if (empty == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+  }
+
+formFill.addEventListener("submit", event => {
+    event.preventDefault();
+    saveUserPref();
+    applyUserPref();
+});
+
+function saveUserPref() {
+    localStorage.setItem("userName", userName.value)
+    localStorage.setItem("bgColor", bgColor.value)
+    localStorage.setItem("txColor", txColor.value)
+}
+
+function applyUserPref() {
+    document.body.style.background = localStorage.getItem("bgColor");
+    document.body.style.color = localStorage.getItem("txColor");
+    txColor.value = localStorage.getItem("txColor")
+}
